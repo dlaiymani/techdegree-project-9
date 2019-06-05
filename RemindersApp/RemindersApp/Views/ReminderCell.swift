11a8b65f0aa55b2ded7bc934eecb9645ae35c008
David laiymani <david.laiymani@gmail.com>
2019-06-05 10:07:52 +0200

    Initial commit

---
+++ RemindersApp/RemindersApp/Views/ReminderCell.swift
@@ -0,0 +1,25 @@
+//
+//  ReminderCell.swift
+//  RemindersApp
+//
+//  Created by davidlaiymani on 05/06/2019.
+//  Copyright © 2019 davidlaiymani. All rights reserved.
+//
+
+import UIKit
+
+class ReminderCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
